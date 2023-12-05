c0ce3f9cc3db8643375b6a8c60cfdb6060758668
Dhwanit Jethava <96478746+dhwanitjethava@users.noreply.github.com>
2023-12-05 17:52:53 +0530

    Update 2. COPY INTO.sql

--- SnowPro Core COF-C02/Nikolai Schuler/2. Data Loading & Unloading/2. COPY INTO.sql
+++ SnowPro Core COF-C02/Nikolai Schuler/2. Data Loading & Unloading/2. COPY INTO.sql
@@ -1,1 +1,30 @@
+-- Create ORDERS table
+CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.ORDERS (
+    ORDER_ID VARCHAR(30),
+    AMOUNT INT,
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30)
+    );
 
+SELECT * FROM OUR_FIRST_DB.PUBLIC.ORDERS;
+
+-- List files contained in stage
+LIST @MANAGE_DB.EXTERNAL_STAGES.aws_stage;
+
+-- COPY command with fully qualified stage object name
+COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS
+    FROM @MANAGE_DB.EXTERNAL_STAGES.aws_stage
+    FILE_FORMAT = (TYPE = CSV
+                   FIELD_DELIMITER = ','
+                   SKIP_HEADER = 1)
+    FILES = ('OrderDetails.csv');
+
+-- COPY command with pattern for file names
+COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS
+    FROM @MANAGE_DB.EXTERNAL_STAGES.aws_stage
+    FILE_FORMAT = (TYPE = CSV
+                   FIELD_DELIMITER = ','
+                   SKIP_HEADER = 1)
+    PATTERN = '.*Order.*';
