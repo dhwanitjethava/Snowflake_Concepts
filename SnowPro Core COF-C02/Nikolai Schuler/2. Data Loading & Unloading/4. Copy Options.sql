94fcbcd86e7f32e08eac37edfa9e0114903ee0e6
Dhwanit Jethava <96478746+dhwanitjethava@users.noreply.github.com>
2023-12-06 12:00:04 +0530

    Update 4. Copy Options.sql

--- SnowPro Core COF-C02/Nikolai Schuler/2. Data Loading & Unloading/4. Copy Options.sql
+++ SnowPro Core COF-C02/Nikolai Schuler/2. Data Loading & Unloading/4. Copy Options.sql
@@ -1,1 +1,97 @@
--- 
+-- Create new stage
+CREATE OR REPLACE STAGE MANAGE_DB.external_stages.aws_stage_errorex
+    URL = 's3://bucketsnowflakes4';
+ 
+-- List files in stage
+LIST @MANAGE_DB.external_stages.aws_stage_errorex;
+
+-- Create example table
+CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.ORDERS_EX (
+    ORDER_ID VARCHAR(30),
+    AMOUNT INT,
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30)
+    );
+
+-- Demonstrating error message
+COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS_EX
+    FROM @MANAGE_DB.external_stages.aws_stage_errorex
+    FILE_FORMAT = (TYPE = CSV FIELD_DELIMITER = ',' SKIP_HEADER = 1)
+    FILES = ('OrderDetails_error.csv');
+
+-- Validating table is empty  
+SELECT * FROM OUR_FIRST_DB.PUBLIC.ORDERS_EX  ;  
+
+-- Error handling using the ON_ERROR option
+COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS_EX
+    FROM @MANAGE_DB.external_stages.aws_stage_errorex
+    FILE_FORMAT = (TYPE = CSV FIELD_DELIMITER = ',' SKIP_HEADER = 1)
+    FILES = ('OrderDetails_error.csv')
+    ON_ERROR = 'CONTINUE';
+    
+-- Validating results and truncating table 
+SELECT * FROM OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+TRUNCATE TABLE OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+
+-- Error handling using the ON_ERROR default option
+COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS_EX
+    FROM @MANAGE_DB.external_stages.aws_stage_errorex
+    FILE_FORMAT = (TYPE = CSV FIELD_DELIMITER = ',' SKIP_HEADER = 1)
+    FILES = ('OrderDetails_error.csv')
+    ON_ERROR = 'ABORT_STATEMENT';
+
+-- Validating results and truncating table 
+SELECT * FROM OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+TRUNCATE TABLE OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+
+-- Error handling using the ON_ERROR = SKIP_FILE
+COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS_EX
+    FROM @MANAGE_DB.external_stages.aws_stage_errorex
+    FILE_FORMAT = (TYPE = CSV FIELD_DELIMITER = ',' SKIP_HEADER = 1)
+    FILES = ('OrderDetails_error.csv', 'OrderDetails_error2.csv')
+    ON_ERROR = 'SKIP_FILE';
+
+-- Validating results and truncating table
+SELECT * FROM OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+TRUNCATE TABLE OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+
+-- Error handling using the ON_ERROR = SKIP_FILE_<number>
+COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS_EX
+    FROM @MANAGE_DB.external_stages.aws_stage_errorex
+    FILE_FORMAT = (TYPE = CSV FIELD_DELIMITER = ',' SKIP_HEADER = 1)
+    FILES = ('OrderDetails_error.csv', 'OrderDetails_error2.csv')
+    ON_ERROR = 'SKIP_FILE_2';
+  
+-- Validating results and truncating table 
+SELECT * FROM OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+TRUNCATE TABLE OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+
+-- Error handling using the ON_ERROR = SKIP_FILE_<number>%
+COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS_EX
+    FROM @MANAGE_DB.external_stages.aws_stage_errorex
+    FILE_FORMAT = (TYPE = CSV FIELD_DELIMITER = ',' SKIP_HEADER = 1)
+    FILES = ('OrderDetails_error.csv', 'OrderDetails_error2.csv')
+    ON_ERROR = 'SKIP_FILE_3%';
+
+-- Validating results and truncating table 
+SELECT * FROM OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+TRUNCATE TABLE OUR_FIRST_DB.PUBLIC.ORDERS_EX;
+
+-- Error handling using the SIZE_LIMIT option
+CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.ORDERS_EX (
+    ORDER_ID VARCHAR(30),
+    AMOUNT INT,
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30)
+    );
+
+COPY INTO OUR_FIRST_DB.PUBLIC.ORDERS_EX
+    FROM @MANAGE_DB.external_stages.aws_stage_errorex
+    FILE_FORMAT = (TYPE = CSV FIELD_DELIMITER = ',' SKIP_HEADER = 1)
+    FILES = ('OrderDetails_error.csv')
+    ON_ERROR = SKIP_FILE_3
+    SIZE_LIMIT = 30;
