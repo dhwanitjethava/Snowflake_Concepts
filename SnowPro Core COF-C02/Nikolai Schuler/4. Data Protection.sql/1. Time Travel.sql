ac597afd4a389ac0a0dc68e556ee1ee54d3417ef
Dhwanit Jethava <96478746+dhwanitjethava@users.noreply.github.com>
2023-12-08 09:58:38 +0530

    Update 1. Time Travel.sql

--- SnowPro Core COF-C02/Nikolai Schuler/4. Data Protection.sql/1. Time Travel.sql
+++ SnowPro Core COF-C02/Nikolai Schuler/4. Data Protection.sql/1. Time Travel.sql
@@ -1,12 +1,12 @@
 -- Setting up table
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.test (
     id int,
-    first_name string,
-    last_name string,
-    email string,
-    gender string,
-    Job string,
-    Phone string
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    Phone STRING
     );
 
 -- Create file format
@@ -51,12 +51,12 @@ SELECT CURRENT_TIMESTAMP;
 -- Preparing table
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.test (
    id int,
-   first_name string,
-    last_name string,
-    email string,
-    gender string,
-    Phone string,
-    Job string
+   first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Phone STRING,
+    Job STRING
     );
 
 -- Load data into table
@@ -73,3 +73,101 @@ UPDATE OUR_FIRST_DB.public.test
 SELECT * FROM OUR_FIRST_DB.public.test;
 
 SELECT * FROM OUR_FIRST_DB.public.test BEFORE (statement => 'query_id');
+
+------------------------------------------------------------------------------------------------------------------------
+
+---- UNDROP objects ----
+
+-- Setting up table
+CREATE OR REPLACE TABLE OUR_FIRST_DB.public.test (
+    id int,
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    Phone STRING
+    );
+
+-- Load data into table
+COPY INTO OUR_FIRST_DB.public.test
+    FROM @MANAGE_DB.external_stages.time_travel_stage
+    FILES = ('customers.csv');
+
+SELECT * FROM OUR_FIRST_DB.public.test;
+
+-- USE-CASE : Update data (by mistake)
+
+UPDATE OUR_FIRST_DB.public.test
+    SET LAST_NAME = 'Tyson';
+
+UPDATE OUR_FIRST_DB.public.test
+    SET JOB = 'Data Analyst';
+
+SELECT * FROM OUR_FIRST_DB.public.test BEFORE (STATEMENT => 'query_id');
+
+-- Restoring (Good method)
+CREATE OR REPLACE TABLE OUR_FIRST_DB.public.test_backup AS
+SELECT * FROM OUR_FIRST_DB.public.test BEFORE (STATEMENT => '01ab3a59-0001-079e-0003-a4c60002d0ee');
+
+TRUNCATE OUR_FIRST_DB.public.test;
+
+INSERT INTO OUR_FIRST_DB.public.test
+SELECT * FROM OUR_FIRST_DB.public.test_backup;
+
+SELECT * FROM OUR_FIRST_DB.public.test ;
+
+-- Undrop preparation
+
+-- Setting up table
+CREATE OR REPLACE STAGE MANAGE_DB.external_stages.time_travel_stage
+    URL = 's3://data-snowflake-fundamentals/time-travel/'
+    FILE_FORMAT = MANAGE_DB.file_formats.csv_file;
+    
+CREATE OR REPLACE TABLE OUR_FIRST_DB.public.customers (
+    id int,
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    phone STRING
+    );
+
+COPY INTO OUR_FIRST_DB.public.customers
+    FROM @MANAGE_DB.external_stages.time_travel_stage
+    EXTERNAL_TABLE_FILES = ('customers.csv');
+
+SELECT * FROM OUR_FIRST_DB.public.customers;
+
+-- Undrop command - Tables
+DROP TABLE OUR_FIRST_DB.public.customers;
+
+SELECT * FROM OUR_FIRST_DB.public.customers;
+
+UNDROP TABLE OUR_FIRST_DB.public.customers;
+
+-- UNDROP command - Schemas
+DROP SCHEMA OUR_FIRST_DB.public;
+
+SELECT * FROM OUR_FIRST_DB.public.customers;
+
+UNDROP SCHEMA OUR_FIRST_DB.public;
+
+SELECT * FROM customers;
+
+-- UNDROP command - Database
+
+DROP DATABASE OUR_FIRST_DB;
+
+SELECT * FROM OUR_FIRST_DB.public.customers;
+
+UNDROP DATABASE OUR_FIRST_DB;
+
+-- Undroping a with a name that already exists
+SELECT * FROM OUR_FIRST_DB.public.customers;
+
+UNDROP table OUR_FIRST_DB.public.customers;
+
+ALTER TABLE OUR_FIRST_DB.public.customers
+RENAME TO OUR_FIRST_DB.public.customers_new;
